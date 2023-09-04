9531cbc777b10b7ca874c78830002791ecc8a78b
Stanley.js <jamais.stanley@gmail.com>
2023-09-04 23:08:28 +0200

    First Commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () =>{
+
+    return (
+        <>
+           {/*Navigation bar at header <Navbar/>*/}
+            <div className="Head">
+                <h1>Saline Academie</h1>
+            </div>
+            {/*Searchbar with the data linked to it <Search/>*/}
+            {/*Footerbar at the footer <Foobar/>*/}
+        </>
+    )
+}
+
+export default Home;
