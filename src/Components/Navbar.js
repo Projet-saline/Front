4f6a35b7af81fb8897c5df872b0c33641e5037ea
Stanley.js <jamais.stanley@gmail.com>
2023-09-06 04:28:23 +0200

    Better styled navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,12 @@
 import React from "react";
-import { useState, useEffect } from 'react';
-
 
 
 const Navbar = () =>{
 
-    const [showNavbar, setShowNavbar] = useState(true);
-
-    const handleScroll = () => {
-        if (window.scrollY > 110) {
-            setShowNavbar(false);
-        } else {
-            setShowNavbar(true);
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-        window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
 
     return(
         <>
-            <div className={`${showNavbar ? 'Navbar-Hero' : 'Navbar-All'}`}>
+            <div className="Navbar-All">
                 <div className="Logo">
                     <img src="https://assets.website-files.com/61ae28f54843d30203fad6db/61c20adcfff22fd7c76d8911_Group%202620.svg" alt="Logo"/>
                 </div>
