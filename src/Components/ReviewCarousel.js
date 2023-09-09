17cdb6c8444adee851305b6ab7333cf4afee74a6
Stanley.js <jamais.stanley@gmail.com>
2023-09-09 19:21:12 +0200

    Readability of Carousel's logic

--- src/Components/ReviewCarousel.js
+++ src/Components/ReviewCarousel.js
@@ -4,67 +4,72 @@ import Star from "../assets/star.svg";
 
 const ReviewCarousel = ({ items }) => {
 
-  // A supprimer à terme le tableau ci-dessous
-
-  
-items = [
-  {
-      name: "John",
-      title: "super",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
-      rating: 5,
-      face: "John.jpg"
-  },
-  {
-      name: "Pauline",
-      title: "bof",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
-      rating: 3,
-      face: "Pauline.jpg"
-
-  },
-  {
-      name: "Pierre",
-      title: "Yes",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
-      rating: 1,
-      face: "John.jpg"
-  },
-  {
-      name: "Celine",
-      title: "J'aime react putain",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
-      rating: 4,
-      face: "Pauline.jpg"
-
-  },
-];
-
-  
   const [currentIndex, setCurrentIndex] = useState(0);
   const [startX, setStartX] = useState(null);
 
-  const handleTouchStart = (e) => {
-    setStartX(e.touches[0].clientX);
+
+  // En gros Touches est le tableau qui compte la position du doigt sur l'écran
+  // clientX est la position du doigt sur l'écran en pixels
+  const handleTouchStart = (event) => {
+    setStartX(event.touches[0].clientX);
   };
 
-  const handleTouchEnd = (e) => {
-    if (!startX) return; 
+  // Le handler pour l'enlèvement du doigt de l'écran qui va swiper le carrousel
+  const handleTouchEnd = (event) => {
 
-    const deltaX = e.changedTouches[0].clientX - startX;
+    // Ici le delta qui calcul l'avancée du doit sur l'écran par rapport a startX
+    const deltaX = event.changedTouches[0].clientX - startX;
 
-    if (deltaX > 50) {
-      setCurrentIndex((prevIndex) => 
-        prevIndex <= 0 ? items.length - 2 : prevIndex - 2
-      );
-    } else if (deltaX < -50) {
-      setCurrentIndex((prevIndex) => 
-        prevIndex >= items.length - 2 ? 0 : prevIndex + 2
-      );
-    }
+    //Testons savoir si le doigt est bien sur l'écran
+    switch (startX) {
+      //Non le doigt n'est pas sur l'écran
+      case null:
+        break;
+      //Oui le doigt est sur l'écran
+      default:
+        //Testons savoir si le doigt a bougé
+        switch (deltaX) {
+          //Oui le doigt a bougé de plus de 50px vers la droite
+          case deltaX > 50:
+            setCurrentIndex((prevIndex) =>
+              prevIndex <= 0 ? items.length - 2 : prevIndex - 2
+            );
+            break;
+          //Oui le doigt a bougé de plus de 50px vers la gauche
+          case deltaX < -50:
+            setCurrentIndex((prevIndex) =>
+              prevIndex >= items.length - 2 ? 0 : prevIndex + 2
+            );
+            break;
+          //Le doigt a fait un mouvement inattendu (moins de 50px, ou vers le haut ou le bas, ou vers la gauche et la droite en même temps ...)
+          default:
+            console.log("Unexpected swipe direction");
+            break;
 
+        };
+        break;
+    };
+    //On remet startX à null pour pouvoir recommencer  
     setStartX(null);
   };
+
+  /* 
+  Commentaire post-ecriture, ce code est assez verbeux je le reconnais, mais il est assez simple a comprendre
+  et il est facilement modifiable pour ajouter des fonctionnalités supplémentaires, comme par exemple un bouton
+  pour passer au slide suivant ou précédent, ou encore un bouton pour passer au slide suivant ou précédent
+  automatiquement toutes les 5 secondes.
+
+  J'ai privilégié la simplicité et la lisibilité du code plutôt que la concision, mais il est tout a fait possible
+  de faire plus concis, mais je pense que ce n'est pas forcément une bonne idée, car le code devient plus difficile
+  a comprendre et a modifier. D'où l'usage du switch case qui est plus verbeux mais plus lisible que des if else imbriqués.
+
+  Inspiré par du code trouvé à droite à gauche je dois l'avoué, mais je l'ai adapté à ma sauce pour qu'il corresponde
+  à mes besoins.
+
+  - Stanley
+
+  PS : J'ai pas use de library pour faire ce carrousel, j'ai tout fait moi même nananana ! :p
+  */
 
   const goToSlide = (idx) => {
     setCurrentIndex(idx * 2);
@@ -72,14 +77,15 @@ items = [
 
   return (
     <div className="Carousel-wrapper">
+      
       <div
         className="Carousel-container"
         onTouchStart={handleTouchStart}
         onTouchEnd={handleTouchEnd}
       >
-        {/* Premier item du carrousel */}
+
         <div className="Carousel-item">
-          <p>Ce que {items[currentIndex].name} penses des formations :</p>
+          <p>{items[currentIndex].name}</p>
           <div className="StarsContainer">
             {[1, 2, 3, 4, 5].map((rating, index) => (
               <img
@@ -90,11 +96,10 @@ items = [
               />
             ))}
           </div>
-          <h4>{items[currentIndex].title}</h4>
           <p>{items[currentIndex].text}</p>
+          <p>{items[currentIndex].date}</p>
         </div>
 
-        {/* Deuxième item du carrousel */}
         {currentIndex + 1 < items.length && ( 
           <div className="Carousel-item">
             <h3>{items[currentIndex + 1].name}</h3>
@@ -113,8 +118,6 @@ items = [
           </div>
         )}
       </div>
-      
-      {/* Indicateurs en bas du carrousel */}
       <div className="Carousel-indicators">
           {Array.from({ length: Math.ceil(items.length / 2) }).map((_, idx) => (
               <div
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,6 +7,39 @@ import SubscriptionModule from "../Components/SubscriptionModule";
 import ReviewCarousel from "../Components/ReviewCarousel";
 import CalltoAction from "../Components/CalltoAction";
 
+const items = [
+    {
+        name: "John",
+        date: "01/01/2021",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
+        rating: 5,
+        face: "John.jpg"
+    },
+    {
+        name: "Pauline",
+        date: "01/01/2021",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
+        rating: 3,
+        face: "Pauline.jpg"
+  
+    },
+    {
+        name: "Pierre",
+        date: "01/01/2021",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
+        rating: 1,
+        face: "John.jpg"
+    },
+    {
+        name: "Celine",
+        date: "01/01/2021",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
+        rating: 4,
+        face: "Pauline.jpg"
+  
+    }
+  ];
+
 const Home = () =>  {
 
     return (
@@ -15,7 +48,7 @@ const Home = () =>  {
             <Hero/>
             <CoursesSection />
             <SubscriptionModule />
-            <ReviewCarousel />
+            <ReviewCarousel items={items} />
             <CalltoAction />
             <Footer/>
         </>
