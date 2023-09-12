9df909953000863f7651d8428836ba39f5609ef7
Stanley.js <jamais.stanley@gmail.com>
2023-09-12 02:39:26 +0200

    Several optimisation on scss + cut done Go Release

--- src/Components/Cut.js
+++ src/Components/Cut.js
@@ -17,7 +17,17 @@ const TextCut = () => {
     return (
         <>
             <div className="TextCutContainer">
-                <p>What if you could dive into the mysteries of music too? Don't just practice your art, force yourself to uncover its secrets. Join our online course and explore the hidden depths of music. Unlock your full musical potential with us.</p>
+                <p>
+                    
+                    What if you could dive into the mysteries of music too? Don't just practice your art, force yourself to uncover its secrets. Join our online course and explore the hidden depths of music. Unlock your full musical potential with us. Our comprehensive program is designed to provide you with the knowledge and skills you need to excel in your musical journey. From theory to practice, we're here to guide you every step of the way, helping you become a true maestro in the world of music.
+
+                    <br/>
+                    <br/>
+                    <br/>
+
+                    Why don't you join the best online courses with world class teachers? We offer a wide range of courses for all levels, from beginner to advanced. Our courses are designed to help you learn at your own pace and in the comfort of your own home. You can choose from a variety of instruments, including piano, guitar, violin, and more. We also offer courses in music theory and composition. So whether you're looking for a new hobby or want to take your skills to the next level, we have something for everyone.
+                </p>
+                <p className="Join Transparent-Button">Join now</p>
             </div>
         
         </>
--- src/Components/SubscriptionModule.js
+++ src/Components/SubscriptionModule.js
@@ -8,7 +8,13 @@ const SubscriptionModule = () => {
                 </div>
                 <div className="Submodule-Content-Right">
                     <h4>Check our prices for the courses you've always dreamt of !</h4>
-                    <p className="Transparent-Button" >See the subscribe page →</p>
+                    <p>
+                        Ready to take the first step in unraveling the musical secrets? 
+                        <br/>
+                        <br/>
+                        Explore our subscription plans and discover the key to unlocking a world of musical excellence.
+                    </p>
+                    <p className="Transparent-Button" >See our plans →</p>
                 </div>
             </div>
         </>
