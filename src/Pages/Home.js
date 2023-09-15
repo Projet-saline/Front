37055bc825d9411acef1626379e5f6fd2b50e07f
Stanley.js <jamais.stanley@gmail.com>
2023-09-15 16:48:14 +0200

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-// import Footer from "../Components/Footer";
-import About from "../Pages/About";
+import Footer from "../Components/Footer";
 import Hero from "../Components/Hero";
 import CoursesSection from "../Components/CoursesSection";
 import SubscriptionModule from "../Components/SubscriptionModule";
